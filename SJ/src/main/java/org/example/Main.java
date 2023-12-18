4b6f712fa5dcb06daffdc061efffd1ac29209ee2
omnicoder9 <nknapp53928@gmail.com>
2023-12-17 20:09:27 -0500

    initial commit

---
+++ SJ/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello world!");
+
+        char data[] = {'a', 'b', 'c'};
+        String str = new String(data);
+
+        System.out.println("abc");
+        String cde = "cde";
+        System.out.println("abc" + cde);
+        String c = "abc".substring(2,3);
+        String d = cde.substring(1, 2);
+
+
+
+    }
+}
