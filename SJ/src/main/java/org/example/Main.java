cc4f499270592ff1c45faed476d41c4d506f0787
omnicoder9 <nknapp53928@gmail.com>
2023-12-20 06:19:08 -0500

    java.nio.charset.Charset

--- SJ/src/main/java/org/example/Main.java
+++ SJ/src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
 
+import java.util.HexFormat;
+
+
+//https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
 public class Main {
     public static void main(String[] args) {
-
-
-
-        System.out.println("Hello world!");
 
         char data[] = {'a', 'b', 'c'};
         String str = new String(data);
@@ -15,6 +15,26 @@ public class Main {
         System.out.println("abc" + cde);
         String c = "abc".substring(2,3);
         String d = cde.substring(1, 2);
+
+        String str1 = new  String();
+        byte[] CDRIVES = HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d");
+        byte[] myvar = "Any String you want".getBytes();
+        String str2 = new String(myvar);
+        String str3 = new String(CDRIVES);
+
+
+
+        //https://www.geeksforgeeks.org/java-nio-charset-charset-class-in-java/
+        String s= "GFG";
+
+        java.nio.charset.Charset charSet = java.nio.charset.Charset.forName("ASCII");
+
+        byte[] byteArr= s.getBytes(charSet);
+        System.out.println("byteArr of \"GFG\" with charsetName \"ASCII\" = " + byteArr);
+        for (byte a : byteArr)
+        {
+            System.out.println(a);
+        }
 
 
 
